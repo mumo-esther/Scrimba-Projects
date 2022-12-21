f675db4be1e3f04c3b6185e3380549f1a788d92c
mumo-esther <mumoesther2021@gmail.com>
2022-12-21 13:20:24 +0300

    passenger counter initialization

---
+++ Passenger Counter/index.js
@@ -0,0 +1,11 @@
+const countEl = document.getElementById("count-el")
+let count = 0;
+function increment() {
+   count = count + 1;
+   countEl.innerText = count;
+}
+
+// save function
+function save() {
+    
+}
